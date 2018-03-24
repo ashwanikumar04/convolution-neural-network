dc254854f0dc697a0e8988897bedb0be3e051873
Ashwani Kumar <ashwanikumar04@gmail.com>
2018-03-24 15:25:09 +0530

    added CNN

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # convolution-neural-network
 A simple image classifier
+<!-- TOC -->
+
+- [convolution-neural-network](#convolution-neural-network)
+    - [Convolution Newral Network](#convolution-newral-network)
+    - [Resources](#resources)
+        - [Papers](#papers)
+        - [Videos](#videos)
+
+<!-- /TOC -->
+## Convolution Newral Network
+
+- Convolution
+    - RELU Layer
+- Max Pooling
+- Flattening
+- Full Connection
+
+
+## Resources
+
+### Papers
+- [Pooling](http://ais.uni-bonn.de/papers/icann2010_maxpool.pdf)
+
+### Videos
+- [Tensorflow and deep learning - without a PhD by Martin Görner](https://www.youtube.com/watch?v=vq2nnJ4g6N0)
+    - [Code](https://github.com/martin-gorner/tensorflow-mnist-tutorial)
---
+++ cnn.py
@@ -0,0 +1,51 @@
+# Convolution Neural Network using Keras
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+
+classifier = Sequential()
+
+# If the input size is increased, it will require more computing power
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Flatten())
+
+# Using units more than 100 gives better results. Convention is to use a power of 2
+classifier.add(Dense(activation='relu', units=128))
+# Activation = "sigmoid" for binary if more than two then use softmax
+classifier.add(Dense(activation='sigmoid', units=1))
+
+# Loss = "binary_crossentropy" for binary if more than two then use "categorical_crossentropy"
+classifier.compile(
+    optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+    '../dataset/training_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+    '../dataset/test_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
+
+classifier.fit_generator(
+    training_set,
+    steps_per_epoch=8000,
+    epochs=25,
+    validation_data=test_set,
+    validation_steps=2000)
